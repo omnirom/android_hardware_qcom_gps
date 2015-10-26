f251f62069e5fdde122848f3605698dfdf0e65c7
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-10-26 17:14:52 +0100

    Fix GPS build on msm8226
    This patch will fix build on msm8226 and provide GPS functionality
    for msm8226 platforms
    
    Change-Id: I84f4c53bc0e3690350c3f815c14d933eaefa0fb3
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
 ifneq ($(filter msm8960 apq8064 ,$(TARGET_BOARD_PLATFORM)),)
     #For msm8960/apq8064 targets
     include $(call all-named-subdir-makefiles,msm8960)
-else ifneq ($(filter msm8974 ,$(TARGET_BOARD_PLATFORM)),)
+else ifneq ($(filter msm8226 msm8974 ,$(TARGET_BOARD_PLATFORM)),)
     #For msm8974 target
     include $(call all-named-subdir-makefiles,msm8974)
 else ifneq ($(filter msm8084 ,$(TARGET_BOARD_PLATFORM)),)
