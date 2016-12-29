651dfb5221b73f2ce1fabeed8e6a74abb73d1205
Jake Whatley <jacob.whatley91@gmail.com>
2016-12-29 21:32:13 +0100

    gps: allow separate CAF directories for GPS HAL
    Change-Id: Icfc21fc4560496070568b9eb03b38d4e8cb2e227

--- Android.mk
+++ Android.mk
@@ -6,6 +6,9 @@ ifneq ($(TARGET_BOARD_AUTO),true)
 
     ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
 
+     ifneq ($(filter caf-msm8992,$(TARGET_QCOM_GPS_VARIANT)),)
+       include $(call all-named-subdir-makefiles,$(TARGET_QCOM_BLUETOOTH_VARIANT))
+     else
       ifneq ($(filter msm8960 apq8064 ,$(TARGET_BOARD_PLATFORM)),)
         #For msm8960/apq8064 targets
         include $(call all-named-subdir-makefiles,msm8960)
@@ -26,7 +29,7 @@ ifneq ($(TARGET_BOARD_AUTO),true)
         GPS_DIRS=core utils loc_api platform_lib_abstractions etc
         include $(call all-named-subdir-makefiles,$(GPS_DIRS))
       endif #TARGET_BOARD_PLATFORM
-
+     endif #TARGET_QCOM_GPS_VARIANT
     else
       ifneq ($(filter msm8909 ,$(TARGET_BOARD_PLATFORM)),)
         #For msm8909 target
